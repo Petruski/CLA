f3fe42e6e2871861aae3bed72ad063ee9d59f6d7
Petrus Söderström <petrus.soderstrom@protonmail.com>
2022-05-01 00:44:45 +0200

    Initial commit

---
+++ include/Statistics.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Petrus Söderström on 2022-05-01.
+//
+
+#include "Statistics.h"
---
+++ src/Statistics.h
@@ -0,0 +1,14 @@
+//
+// Created by Petrus Söderström on 2022-05-01.
+//
+
+#ifndef CLA_STATISTICS_H
+#define CLA_STATISTICS_H
+
+
+class Statistics {
+
+};
+
+
+#endif //CLA_STATISTICS_H
